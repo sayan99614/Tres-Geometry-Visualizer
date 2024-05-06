bf2e4e423dc72e99a56073602c41f2b066f0e3e6
dheeman pati <52071270+sayan99614@users.noreply.github.com>
2024-05-06 20:00:35 +0530

    added geometry control

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <script setup lang="ts">
+import { ref, computed } from 'vue';
 import { TresCanvas } from '@tresjs/core'
-import { OrbitControls } from '@tresjs/cientos'
+import { OrbitControls } from '@tresjs/cientos';
+import GeometryControl from '@/components/GeometryControl.vue';
+const geometry = ref('BoxGeometry');
+const geoMetryProps= ref({width:1,height:2,depth:5});
+const selectedGeometry = computed(()=>`Tres${geometry.value}`)
+const changeGeometry = ( type) => {
+  geometry.value = type;
+}
 </script>
 
 <template>
-  <div class="tres-canvas">
   <TresCanvas
     clear-color="#82DBC5"
+    window-size
   >
     <TresPerspectiveCamera
       :look-at="[0, 0, 0]"
     />
+    <OrbitControls/>
     <!-- position[x,y,z] -->
-    <TresMesh :position="[0,0,0]" :scale="[1,2,0.5]">    
-      <TresBoxGeometry/>
+    <TresMesh :position="[0,0,0]">
+      <!-- <TresBoxGeometry :args="[geoMetryProps.width*1, geoMetryProps.height*1, geoMetryProps.depth*1]"/>     -->
+      <component :is="selectedGeometry" :args="[geoMetryProps.width*1, geoMetryProps.height*1, geoMetryProps.depth*1]" />
       <TresMeshNormalMaterial />
     </TresMesh>
     <TresAxesHelper/>
   </TresCanvas>
-</div>
-  <button>Click me</button>
+  <GeometryControl @change-geometry="changeGeometry" v-model:width="geoMetryProps.width" v-model:height="geoMetryProps.height" v-model:depth="geoMetryProps.depth" />
 </template>
-
 
 <style>
 .tres-canvas {
---
+++ src/components/GeometryControl.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from 'vue'
+const geometryTypes = ref(['BoxGeometry', 'CircleGeometry', 'ConeGeometry', 'CylinderGeometry', 'DodecahedronGeometry', 'IcosahedronGeometry', 'OctahedronGeometry', 'PlaneGeometry', 'RingGeometry', 'SphereGeometry', 'TetrahedronGeometry', 'TorusGeometry', 'TorusKnotGeometry'])
+const height= defineModel('height');
+const width= defineModel('width');
+const depth= defineModel('depth');
+const emit= defineEmits(['changeGeometry']);
+
+
+
+</script>
+
+<template>
+  <div class="relative">
+    <div class="absolute card">
+        <div>
+            <label for="geometry">GeometryTypes</label>
+            <select id="geometry"
+                v-model="geometry"
+                @change="()=>emit('change-geometry',geometry)"
+            >
+                <option v-for="type in geometryTypes" :key="type" :value="type">{{ type }}</option>
+            </select>    
+        </div>
+        <div>
+          <label for="geometry">Height</label>
+          <input type="range" v-model="height" min="1" max="30">
+          {{height}}
+          </div>
+
+          <div>
+            <label for="">Width</label>
+            <input type="range" v-model="width" min="1" max="30">
+            {{width}}
+            </div>
+
+            <div>
+            <label for="">Depth</label>
+            <input type="range" v-model="depth" min="1" max="30">
+            {{depth}}
+            </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.relative {
+  position: relative;
+}
+.absolute {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: #fff;
+}
+.card{
+    padding: 1rem;
+    border-radius: 0.5rem;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    background-color: #fff;
+    z-index: 100;
+    opacity: 50%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: start;
+}
+/* .card > div{
+    margin-bottom: 0.5rem;
+} */
+</style>
